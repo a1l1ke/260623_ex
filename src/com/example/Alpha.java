9803677028982d7600c50be80770312a8ed56e91
a1l1ke <onetenthplease@gmail.com>
2026-06-23 14:45:43 +0900

    feat: 필드 접근 제어자 예제 확장

--- src/com/example/Alpha.java
+++ src/com/example/Alpha.java
@@ -11,9 +11,21 @@ public class Alpha {
     // airplace 패키지 외부 접근
     public Airplane airplane = new Airplane();
     Beta beta = new Beta(); // X
+
+    public static void main(String[] args) {
+        Alpha alpha = new Alpha();
+        System.out.println(alpha.airplane);
+        System.out.println(alpha.beta); // 나는 베타도 노출하고 싶지 않아요 내가 혼자만 쓰고 싶어요
+
+//        System.out.println(alpha.airplane.wing); // private
+        System.out.println(alpha.airplane.wing2); // default - 같은 경로면 접근 가능
+        System.out.println(alpha.airplane.wing3); // public - 모두 접근가능
+    }
 }
 
 // 아무 것도 안 붙으면 default
 class Airplane {
-
+    private String wing;
+    String wing2;
+    public String wing3;
 }
