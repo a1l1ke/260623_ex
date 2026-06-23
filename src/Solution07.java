23436fe1970893c5619711cb1e8d221691df97cf
a1l1ke <onetenthplease@gmail.com>
2026-06-23 14:37:46 +0900

    feat: 패키지와 접근 제어자 예제 추가

---
+++ src/Solution07.java
@@ -0,0 +1,10 @@
+//import com.example.Airplane;
+
+import com.example.Alpha;
+
+public class Solution07 {
+    public static void main(String[] args) {
+        Alpha alpha = new Alpha(); // public 접근제어
+//        Airplane airplane = new Airplane(); // default 접근제어
+    }
+}
---
+++ src/com/example/Alpha.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Alpha {
+    // 기본 생성자는 public
+//    Alpha() {
+    public Alpha() {
+        Airplane airplane = new Airplane(); // default 접근제어
+    }
+}
+
+// 아무 것도 안 붙으면 default
+class Airplane {
+
+}
