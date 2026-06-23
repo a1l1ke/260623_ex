1ddeb72e19e225392c2e04cc920c53cef4646ac1
a1l1ke <onetenthplease@gmail.com>
2026-06-23 14:40:53 +0900

    feat: 클래스와 멤버 접근 제어 예제 확장

--- src/Solution07.java
+++ src/Solution07.java
@@ -1,10 +1,16 @@
 //import com.example.Airplane;
 
 import com.example.Alpha;
+//import com.example.Beta;
 
 public class Solution07 {
     public static void main(String[] args) {
+        // 클래스 - 접근 제어자 차이
         Alpha alpha = new Alpha(); // public 접근제어
 //        Airplane airplane = new Airplane(); // default 접근제어
+//        Beta beta = new Beta();
+        // 멤버변수 - 접근 제어자 차이
+        System.out.println("alpha.airplane = " + alpha.airplane);
+//        alpha.beta
     }
 }
--- src/com/example/Alpha.java
+++ src/com/example/Alpha.java
@@ -4,8 +4,13 @@ public class Alpha {
     // 기본 생성자는 public
 //    Alpha() {
     public Alpha() {
-        Airplane airplane = new Airplane(); // default 접근제어
+//        Airplane airplane = new Airplane(); // default 접근제어
+//        Beta beta = new Beta(); // 같은 패키지 내부라서 (public 안 붙어도)
     }
+
+    // airplace 패키지 외부 접근
+    public Airplane airplane = new Airplane();
+    Beta beta = new Beta(); // X
 }
 
 // 아무 것도 안 붙으면 default
